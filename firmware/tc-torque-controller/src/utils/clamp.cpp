79a036b57c800bd75af78ed3360122a2ba67db27
Jake Read <jake.read@cba.mit.edu>
2021-03-31 21:37:03 -0400

    rm closed loop stepper fw

--- firmware/tc-torque-controller/src/utils/clamp.cpp
+++
@@ -1,9 +0,0 @@
-#include "clamp.h"
-
-void clamp(float *val, float min, float max){
-    if(*val < min){
-        *val = min;
-    } else if (*val > max){
-        *val = max;
-    }
-}
--- firmware/tc-torque-controller/src/utils/clamp.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLAMP_H_
-#define CLAMP_H_ 
-
-void clamp(float *val, float min, float max);
-
-#endif 
